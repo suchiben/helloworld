082f8ddec852bf5a58c8e3b4bf82815742cfb47b
SIT-100110 <SIT-100110@DESKTOP-4V3LI23>
2020-01-30 12:53:47 -0600

    to demo

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter int");
+		int n= sc.nextInt();
+		
+
+	}
+
+}
